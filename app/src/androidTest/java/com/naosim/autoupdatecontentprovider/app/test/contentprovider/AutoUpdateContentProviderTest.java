0208b22e9da7877301d1e0ef5d2418e953791089
naosim <nao_pillows@yahoo.co.jp>
2014-05-24 11:32:36 +0900

    テスト追加

---
+++ app/src/androidTest/java/com/naosim/autoupdatecontentprovider/app/test/contentprovider/AutoUpdateContentProviderTest.java
@@ -0,0 +1,20 @@
+package com.naosim.autoupdatecontentprovider.app.test.contentprovider;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by nao_pillows on 2014/05/24.
+ */
+@RunWith(RobolectricTestRunner.class)
+public class AutoUpdateContentProviderTest {
+    @Test
+    public void testhoge() throws Exception {
+        assertTrue(true);
+
+    }
+}
